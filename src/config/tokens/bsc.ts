5a475138e16a91cfc1a23a2b00dcc478e39fed32
unknown <faizanbabar70@yahoo.com>
2023-06-20 18:01:36 +0500

    Add fixes

--- src/config/tokens/bsc.ts
+++ src/config/tokens/bsc.ts
@@ -2,7 +2,7 @@ import { ChainId, Token } from '@sushiswap/core-sdk'
 
 export const DAI = new Token(ChainId.BSC, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const USD = new Token(ChainId.BSC, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
-export const USDC = new Token(ChainId.BSC, '0x526f6E8ada6d0ec42Bd9dd985c2c171d3EB97f84', 18, 'USDC', 'USD Coin')
+export const USDC = new Token(ChainId.BSC, '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', 18, 'USDC', 'USD Coin')
 export const USDT = new Token(ChainId.BSC, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD')
 export const BTCB = new Token(ChainId.BSC, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Bitcoin')
 export const WETH = new Token(ChainId.BSC, '0x2170Ed0880ac9A755fd29B2688956BD959F933F8', 18, 'WETH', 'Wrapped Ether')
