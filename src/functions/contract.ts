d72377b98cdf1b230c181d706bb4b9811aef5d00
unknown <faizanbabar70@yahoo.com>
2023-06-21 14:20:54 +0500

    Add fixes

--- src/functions/contract.ts
+++ src/functions/contract.ts
@@ -35,7 +35,7 @@ export function getRouterAddress(chainId?: ChainId) {
     : chainId == 137
     ? '0xc684e9b1Bf80955822BC1B4512049e4897a95631'
     : chainId == 56
-    ? '0xD2f5b0A53e15c9E550A094dC4B648B324A153A6B'
+    ? '0x9b9577d046cfA15d5D7cFADeB562D3f589357b73'
       //@ts-ignore
     : ROUTER_ADDRESS[chainId]
 }
