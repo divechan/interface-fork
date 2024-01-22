765f29248995a2f8273500ee7a463fa6f73de47e
Neville Divecha <neville.divecha@steamexchange.ca>
2024-01-22 13:03:45 -0500

    UI changes

--- src/components/Header/useMenu.tsx
+++ src/components/Header/useMenu.tsx
@@ -37,7 +37,7 @@ const useMenu: UseMenu = () => {
 
     const menu: Menu = [{
       key: 'Home',
-      title: i18n._(t`Home`),
+      title: i18n._(t`Swap`),
       link: '/swap',
     },
     ]
@@ -57,13 +57,8 @@ const useMenu: UseMenu = () => {
 
     const trade = [
       {
-        key: 'swap',
-        title: i18n._(t`Trade`),
-        link: '/swap',
-      },
-      {
         key: 'buy',
-        title: i18n._(t`Buy Crypto`),
+        title: i18n._(t`Onramper`),
         link: 'https://buy.onramper.com/?apiKey=pk_prod_01H5AXWF5H767QV1SA4Z3RRPWE',
       },
     ]
@@ -144,12 +139,7 @@ const useMenu: UseMenu = () => {
 
       }
     } else {
-      menu.push({
-        key: 'trade',
-        title: i18n._(t`Digital Assets`),
-        icon: <SwitchVerticalIcon width={20} />,
-        items: trade,
-      })
+
       menu.push({
         key: 'explorers',
         title: i18n._(t`Explorers`),
@@ -162,6 +152,12 @@ const useMenu: UseMenu = () => {
         title: i18n._(t`Manage Liquidity`),
         icon: <BeakerIcon width={20} />,
         items: liquidity.filter((item) => !item?.disabled),
+      })
+      menu.push({
+        key: 'trade',
+        title: i18n._(t`Buy Crypto`),
+        icon: <SwitchVerticalIcon width={20} />,
+        items: trade,
       })
     }
 
