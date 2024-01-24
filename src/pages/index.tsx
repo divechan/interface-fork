0849e729334f2803d7e458c248412e2256b98a52
unknown <faizanbabar70@yahoo.com>
2024-01-24 21:46:29 +0500

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import Container from 'app/components/Container'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function Dashboard() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push("/swap")
+    // @ts-ignore TYPE NEEDS FIXING
+   
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <Container id="dashboard-page" className="py-4 mx-auto md:py-8 lg:py-12" maxWidth="2xl">
       <Head>
