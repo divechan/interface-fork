4af8b440e66b0d271a93a84d79635b0808ef81f5
Neville Divecha <neville.divecha@steamexchange.ca>
2024-01-24 14:22:14 -0500

    fix usdc

--- src/config/tokens/rails_testnet.ts
+++ src/config/tokens/rails_testnet.ts
@@ -1,7 +1,7 @@
 import { ChainId, Token } from '@sushiswap/core-sdk'
 import { RAILS_TESTNET } from 'app/constants'
 
-export const USDC = new Token(RAILS_TESTNET, '0x0000000000000000000000000000000000627802', 6, 'USDC', 'USD Coin')
+export const USDC = new Token(RAILS_TESTNET, '0x0000000000000000000000000000000000627801', 6, 'USDC', 'USD Coin')
 export const WETH = new Token(
   RAILS_TESTNET,
   '0x0000000000000000000000000000000000627800',
